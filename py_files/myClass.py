f3f903b6a74b4ac12bba9bd285e4ce1b90bef1fc
pajjaecat <pajjaecat@gmail.com>
2022-12-10 00:39:37 +0000

    Finish Pers

---
+++ py_files/myClass.py
@@ -0,0 +1,255 @@
+import pandapower, pandas, numpy, ipyparallel
+
+pd = pandas
+np = numpy 
+pp = pandapower
+ipp = ipyparallel
+
+##############################         Class          #########################################
+class CreateParEngines:
+    
+    def __init__(self, n_engines:int=0):
+        """
+        Create n parallel engines
+        
+        """
+        cluster = ipp.Cluster(n=n_engines)     # Create cluster
+        cluster.start_cluster_sync()            # Start cluster
+        self.rc = cluster.connect_client_sync() # connect client
+        self.rc.wait_for_engines(n_engines)     # Wait for engines to be available for all clients
+        self.dview = self.rc[:]                 # Create a direct view of all engine or clients that would
+                                                # be used to command each client
+
+    def sendVar_to_localSpace(self, 
+                              run_periodIndex: pandas.core.indexes.period.PeriodIndex, 
+                              opf_status, 
+                              dict_df_sgenLoad:dict, 
+                              parameters_dict: dict, 
+                              nb_exec: int ):
+        """
+        Send variables to the local space of each parallem engine. 
+        
+        
+        Parameters
+        -----------
+        run_periodIndex:
+            Total number of periods to run simulation for. The number of period each engine 
+            will work on is therfore given by len(run_periodIndex)/n where n is the nuber of 
+            engines
+        opf_status : Optimal power flow status. 2 values are possible
+            False (Boolean) ==> Run power flow
+            'Both' (str)    ==> Run optimal power flow or power flow depending on the situation
+        dict_df_sgenLoad: (dict)
+        
+        parameters_dict:
+        
+        nb_exec: execution number in the current loop
+            0 ==>> Clear the localspace of all engines
+        
+        """
+        # Raise Error if the OPF type is not well defined 
+        if opf_status not in ['Both', False]:
+             raise valueError ('<opf_status> must be either of [False, ''Both'']')
+        
+        self.run_periodIndex = run_periodIndex;
+        self.pred_model = parameters_dict['pred_model']
+        self.opf_status = opf_status;
+        
+        if nb_exec == 0 : 
+            # Clear the localspace of all engines
+            self.dview.clear() 
+
+            # # Import following modules on the local space of clients or engines
+            with self.rc[:].sync_imports():
+                import numpy, pandapower,pandas, Par_myFunctions
+
+        # Share the total number of period in df_prodHT.index among all the created engines
+        self.dview.scatter('period_part', run_periodIndex)
+
+        # Add all variables in the parameters_dict  to  the local space of each client
+        self.dview.push(parameters_dict) 
+
+        # Send following Variables to local space of parallel engines
+        self.dview['opf_status'] = opf_status  
+        self.dview['dict_df_sgenLoad'] = dict_df_sgenLoad
+        
+        
+        
+    def gather_results(self, par_result_name:str):
+        """
+        Gather the results of parallel computing and output a unique  
+        variable
+        
+        Parameters
+        ----------
+        par_result_name: (string)
+            Name use to call the parallel running in block pf_opf
+        
+        """
+        # Che9k if the given argument is a string
+        if type(par_result_name) is not str: 
+            raise TypeError('gather_results() can only handle string as argument')
+        
+        self.gathered_results = self.dview.gather(par_result_name)
+        
+        return self.gathered_results
+    
+    
+    
+    def get_results_asDf(self):
+        """
+        Extract and save the result of the parallel computation in a dataframe that is output
+
+        Output:
+        ---------
+        Dataframe as:
+            max_vm_pu : Maximum voltage recorded over all the bus at the instant given 
+            by the df.index
+            Other columns : THe injected power of the respective HT producers.
+
+
+        """
+
+        # Get df_prodHT colums name [] from one of the engines 
+        df_prodHT_colName = self.dview['dict_df_sgenLoad'][-1]['df_prodHT'].columns
+        
+        # Get all the elements from the parallel result in a list
+        # elm[0]   : Maximum voltage on all the line 
+        # elm[1][0]: Power injected into the network by the first HT producer 
+        # ...
+        # elm[1][n]: Power injected into the network by the last HT producer i.e. P0100 
+        # elm[2]   : Period index associated to all the previous output variable
+
+        # elm[0] can either be a list of [max_vm_pu_pf : max voltage  before opf
+        #                                 max_vm_pu : maximum voltage after opf] 
+        # or a single float which is  max_vm_pu : maximum voltage after opf. 
+        # See the function run_powerflow_at (*args, ofp_status='both', pred_model= 'Pers')
+        
+        parallel_result = self.gathered_results
+        
+        if type(parallel_result[0][0]) is list: 
+            sep_list = [(*elm[0], *elm[1], elm[2]) for elm in parallel_result]
+            # Create a colums using 'vm_pu_max' and add the HT producers name
+            colls = ['max_vm_pu_pf', 'max_vm_pu'] + df_prodHT_colName.to_list()
+        else:
+            sep_list = [(elm[0], *elm[1], elm[2]) for elm in parallel_result]
+            # Create a colums using 'vm_pu_max' and add the HT producers name
+            colls = ['max_vm_pu'] + df_prodHT_colName.to_list()
+
+
+        data_input = np.array(np.array(sep_list)[:, :-1], dtype=float)
+        index_list = np.array(sep_list)[:, -1]
+
+        # create new  dataframe based on previous unpack data
+        df = pd.DataFrame(data=data_input, index=index_list, columns=colls)
+
+        # return the newly create dataFrame with the index sorted 
+        return df.sort_index()
+
+
+
+    def __dview__(self):
+        return self.dview
+
+    def __run_periodIndex__(self):
+        return self.run_periodIndex
+    
+    def __pred_model__(self):
+        return self.pred_model
+    
+    def __opf_status__(self):
+        return self.opf_status
+        
+
+
+    def get_dview(self):
+        return self.__dview__()
+
+    def get_run_periodIndex(self):
+        return self.__run_periodIndex__()
+    
+    def get_pred_model(self):
+        return self.__pred_model__()
+    
+    def get_opf_status(self):
+        return self.__opf_status__()
+
+        
+        
+        
+
+class InitNetwork:
+    
+    def __init__(self, 
+               higherNet:pp.auxiliary.pandapowerNet, 
+               LowerNet:pp.auxiliary.pandapowerNet 
+                ):
+        """
+        Parameters: 
+        -----------
+            higherNet: Higher level network
+            LowerNet : Lower level Network 
+        """
+        self._higherNet = higherNet
+        self._lowerNet  = LowerNet
+        self.__check_network_order__()
+        
+        
+        
+    def __check_network_order__(self):
+        # Make sure the first input i.e. the network is bigger than the second one
+        if len(self._higherNet.bus) < len(self._lowerNet.bus): 
+            raise Exception('The first input (higher net) has a lower number of bus\n'
+                            'compared to the second input (lower network). ')
+    
+
+
+    def __sum_max_p_wm_higherNet__(self):
+        """ Compute the total of BT prod and Load on higher level network """
+        bt_max = self._higherNet.sgen[self._higherNet.sgen.name.isna()].max_p_mw.sum()
+        load_max = self._higherNet.load.max_p_mw.sum()
+        
+        return bt_max, load_max
+        
+        
+    def __sum_max_p_wm_lowerNet__(self):
+        """ Compute the total of BT prod and Load on lower level network """
+        bt_max = self._lowerNet.sgen[self._higherNet.sgen.name.isna()].max_p_mw.sum()
+        load_max = self._lowerNet.load.max_p_mw.sum()
+        
+        return bt_max, load_max
+    
+    
+    def __lowerNet_hv_bus_df__(self):
+        """ Extract higher Voltage bus bus in the lower network: These are 
+            bus for witch vn_kv=20.6 
+        """
+        return self._lowerNet.bus.groupby('vn_kv').get_group(20.6)
+         
+        
+    
+    ###  Define getter    ------------------------------------------------------------------
+    def get_higherNet(self):
+        return self._higherNet
+    
+    def get_lowerNet(self):
+        return self._lowerNet
+    
+    def get_sum_max_p_wm_higherNet(self):
+        return self.__sum_max_p_wm_higherNet__()
+    
+    def get_sum_max_p_wm_lowerNet(self):
+        return self.__sum_max_p_wm_lowerNet__()
+    
+    def get_lowerNet_hv_bus_df(self):
+        return self.__lowerNet_hv_bus_df__()
+    
+    
+        
+        
+        
+        
+
+
+        
+        
